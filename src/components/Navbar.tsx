0883a37f39a5040ecafb5dadaf1f4e17adbf4743
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-23 13:57:54 +0100

    socialmedia icon

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+export default function Navbar() {
+    return (
+      <nav className="navigation">
+        <div
+          className="navigation-menu">
+          <ul>
+            <li>
+                <a href="#" className="fa fa-facebook" ></a>
+            </li>
+            <li>
+                <a href="#" className="fa fa-instagram"></a>
+            </li>
+            <li>
+                <a href="#" className="fa fa-linkedin"></a>
+            </li>
+            <li>
+                <a href="#" className="fa fa-github"></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
