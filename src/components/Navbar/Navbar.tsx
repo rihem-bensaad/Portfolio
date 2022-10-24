1d4583eba87ff6e0b4c1adc7285a63ed263eeea5
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-24 22:17:33 +0100

    links oscial media

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
       <section className="navigation">
   <div className="nav-container">
     <div className="brand">
-      <a href="#!">🍀 Rihem BEN SAAD</a>
+      <a href="#!"> Rihem BEN SAAD</a>
     </div>
     <nav>
   <div className="nav-mobile">
@@ -13,22 +13,22 @@ export default function Navbar() {
   </div>
   <ul className="nav-list">
   <li>
-      <a href="#" className='linkedin'>
+      <a href="https://www.linkedin.com/in/rihem-ben-saad/" className='linkedin' target="_blank">
         <i className='fa fa-linkedin'></i>
       </a>
     </li>
     <li>
-      <a href="#" className='github'>
+      <a href="https://github.com/rihem-bensaad" className='github' target="_blank">
         <i className='fa fa-github'></i>
       </a>
     </li>
     <li>
-      <a href="#" className='facebook'>
+      <a href="https://www.facebook.com/profile.php?id=100006434415288" className='facebook' target="_blank">
         <i className='fa fa-facebook'></i>
       </a>
     </li>
     <li>
-      <a href="#" className='instagram'>
+      <a href="https://www.instagram.com/rihem_b_saad/" className='instagram' target="_blank">
         <i className='fa fa-instagram'></i>
       </a>
     </li>
