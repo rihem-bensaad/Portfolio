40bb1456d49b9a4783d5cb18bf95fd1fc548fc0b
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-24 23:03:49 +0100

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,19 @@
+import './Footer.scss'
 function Footer(){
     return(
         <div>
+            <footer>
+                <ul className="menu">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Partners</a></li>
+                        <li><a href="#">Specialties</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                </ul>
+            <div className="footer-copyright">
+                <p>Copyright @ 2022 All Rights Reserved.</p>
+            </div>
+    </footer>
         </div>
     );
 }
