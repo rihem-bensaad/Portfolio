989337fdec751155a0711d699d31ce930fcd8e24
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-24 17:01:50 +0100

    valid navbar with scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 
 
