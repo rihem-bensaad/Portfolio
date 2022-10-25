82e7c7d297f4d99b8d917489b188db13a3cb98e1
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-25 22:47:34 +0100

    img

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,28 +3,30 @@ function Footer(){
     return(
         <div>
             <footer>
+                  <img src="" alt="" />
             <ul className="footer">
+                
                 <li>
                     <a href="https://www.linkedin.com/in/rihem-ben-saad/" className='linkedin' target="_blank">
                         <i className='fa fa-linkedin'></i>
                     </a>
-                    </li>
-                    <li>
+                </li>
+                <li>
                     <a href="https://github.com/rihem-bensaad" className='github' target="_blank">
                         <i className='fa fa-github'></i>
                     </a>
-                    </li>
-                    <li>
+                </li>
+                <li>
                     <a href="https://www.facebook.com/profile.php?id=100006434415288" className='facebook' target="_blank">
                         <i className='fa fa-facebook'></i>
                     </a>
-                    </li>
-                    <li>
+                </li>
+                <li>
                     <a href="https://www.instagram.com/rihem_b_saad/" className='instagram' target="_blank">
                         <i className='fa fa-instagram'></i>
                     </a>
-                    </li>
-                </ul>
+                </li>
+            </ul>
             <div className="footer-copyright">
                 <p>Copyright @ 2022 All Rights Reserved.</p>
             </div>
