f6ce4342a2e2720cd39070a5793bbf4a2446f2da
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-26 22:22:37 +0100

    icons

--- src/components/File/File.tsx
+++ src/components/File/File.tsx
@@ -2,44 +2,22 @@ import './File.scss';
 
 export default function File() {
     return (
-        <div>
-        <div className="jumbotron">
-      <div className="container">  
-        <div className="main">
-          <h1>We are Broadway</h1>
-          <a href="#" className="btn-main">Get Started</a>
-          
-        </div>
-      </div>
-    </div>
+            <div className="supporting">
+            <div className="container">
+                <div className="col">
+                <h2>Hello everyone I'm Rihem BEN SAAD</h2>
+                <p>Hello world! I am Rihem BEN SAAD from Tunisia and I am currently Graduated as a Full stack JS developer from RBK, a Coding RebootKamp powered by Hack Reactor. I create content in design and development using different libraries and frameworks such as React.js, Angular, Node.js, etc..
+                I have a background in microcomputer system maintenance so I enjoy repairing computers. When I am not developing, you can find me learning things on YouTube, watching series, practice sports. 
+                </p>
+                </div>
 
-    <div className="supporting">
-      <div className="container">
-        <div className="col">
-          <img src="https://s3.amazonaws.com/codecademy-content/projects/broadway/design.svg"/>
-          <h2>Design</h2>
-          <p>Make your projects look great and interact beautifully.</p>
-          <a href="#" className="btn-default">Learn More</a>
-          
-        </div>
-        <div className="col">
-          <img src="https://s3.amazonaws.com/codecademy-content/projects/broadway/develop.svg"/>
-          <h2>Develop</h2>
-          <p>Use modern tools to turn your design into a web site</p>
-          <a href="#" className="btn-default">Learn More</a>
-          
-        </div>
-        <div className="col">
-          <img src="https://s3.amazonaws.com/codecademy-content/projects/broadway/deploy.svg"/>
-          <h2>Deploy</h2>
-          <p>Use modern tools to turn your design into a web site</p>
-          <a href="#" className="btn-default">Learn More</a>
-          
-        </div>
-      </div>
-      <div className="clearfix"></div>
-    </div>
-    </div>
+                <div className="col">
+                    <img src="https://media.istockphoto.com/vectors/teenage-girk-working-on-computer-in-classroom-vector-id1150453750?k=20&m=1150453750&s=612x612&w=0&h=1_UuOCVWdUbtJ09MKzBSXZrK65MmzEZq7rzLSi4WKDU=" alt="" />
+                </div>
+            </div>
+            <div className="clearfix"></div>
+            </div>
+    
 
 
         
