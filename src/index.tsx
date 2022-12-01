bd9ab23b7fcd6d181625111bdcd387360484e001
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-12-01 17:27:08 +0100

    i recat resp

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { useMediaQuery } from 'react-responsive'
 
+const Example = () => {
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
+  const isBigScreen = useMediaQuery({ minWidth: 1824 })
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+  const isPortrait = useMediaQuery({ orientation: 'portrait' })
+  const isRetina = useMediaQuery({ minResolution: '2dppx' })
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
